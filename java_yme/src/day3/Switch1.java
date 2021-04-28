6c4797bfb7ee7771ddd8da87c6ee7cf6909e3d36
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-04-28 17:31:35 +0900

    자바3일차 Switch문 기본 예제1

---
+++ java_yme/src/day3/Switch1.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class Switch1 {
+
+	public static void main(String[] args) {
+		// switch문을 이용한 홀짝 판별 예제
+		int num = 4;
+		
+		switch(num % 2){
+			case 0:
+				System.out.println("짝수");
+				break;
+		default:
+			System.out.println("홀수");
+		}
+	}
+
+}
