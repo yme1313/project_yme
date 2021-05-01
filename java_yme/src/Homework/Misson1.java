ea7cf3969cc7e008d6be8eece11b2a2df19ed870
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-05-01 23:00:55 +0900

    과제 별찍기

--- java_yme/src/Homework/Misson1.java
+++ java_yme/src/Homework/Misson1.java
@@ -3,8 +3,31 @@ package Homework;
 public class Misson1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*	
+		 *          *
+		 *         ***
+		 *        *****
+		 *       *******
+		 *      *********      
+		 *   */
+			
+		/*      *********
+		 *       *******
+		 *        *****
+		 *         ***
+		 *          *
+		 */ 
 
-	}
 
-}
+		for (int i = 0; i < 5; i++) { 
+		    for (int j = 5-1; j > i; j--) { 
+		        System.out.print(" "); 
+		    } 
+		    for (int k = 0; k < 2*i+1; k++) { 
+		        System.out.print("*"); 
+		    } 
+		        System.out.println(); 
+		}
+		} 
+		}
+
