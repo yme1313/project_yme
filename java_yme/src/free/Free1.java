5ff5a3309fcc34627f3f8f5eeb751563f7834244
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-05-05 22:43:21 +0900

    연습장

--- java_yme/src/free/Free1.java
+++ java_yme/src/free/Free1.java
@@ -5,23 +5,34 @@ import java.util.Scanner;
 public class Free1 {
 
 	public static void main(String[] args) {
-		/* 정수가 2의배수, 3의배수 6의배수, 2,3,6의 배수가 아닌지 
-		 * 판별하는 예제를 이중 if문을 이용 하여 작성
+
+		/* 1~10사이의 정수 3개를 랜덤으로 생성하여 배열에 저장한 후
+		 * 생성된 값들이 중복되었는지 안되었는지 확인하는 코드를 작성하세요.
+		 * 예시 : 
+		 * 랜덤 : 3 8 2
+		 * 중복되지 않음
+		 * 예시 : 
+		 * 랜덤 : 2 8 8
+		 * 중복
 		 * */
-		System.out.print("정수를 입력하세요 : ");
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		
-		if(n%2==0) {
-			if(n%3==0) {
-				System.out.println("6의 배수 입니다.");
-			} else  {
-				System.out.println("2의 배수 입니다.");
-			} 
-		}else if(n%3==0) {
-				System.out.println("3의 배수 입니다.");
-			} else {
-				System.out.println("2,3,6의 배수가 아닙니다.");
-			}
-		}
+		/* 1~ 10사이의 정수를 랜덤하게 3개를 생성하는 코드
+		 * 배열에 저장
+		 * 생성된 값들이 중복되었는지 안 되었는지 확인하는 코드
+		 * */
+		int min = 1;
+		int max = 10;
+		//int r = (int)(Math.random() * (max - min + 1) + min);
+		//배열에 저장
+		int arr[] = new int[3];
+		int i, k;
+		/* 1 2 3 4 가 중북되는지 검사
+		 * 12 13 14	i=1 k=i+1
+		 * 23 24	i=2 
+		 * 34 		i=3
+		 * */
+
+		} 
 	}
+
+
+	
