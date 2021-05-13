94f6c9c17eff1c71ec95289485b8db8b16260564
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-05-13 17:40:42 +0900

    가계부 실행

---
+++ java_yme/src/accountbook/AccountBookTest.java
@@ -0,0 +1,21 @@
+package accountbook;
+
+import java.util.Scanner;
+
+public class AccountBookTest {
+
+	public static void main(String[] args) {
+		/* 가계부 프로그램 구현 하려고 한다.
+		 * 이때 필요한 클래스를 만들어 보세요.
+			종류 : 수입 / 지출
+			날짜 : (1~30)
+			자산 : 현금/은행/카드
+			분류 : 교통비 / 식비 등
+			금액 : xx원
+			내역 : 교통비 (버스비)
+		 * */
+	}
+
+
+	
+}
