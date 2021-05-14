3baefbffc0bd277dc00cc3162a5d438af449e657
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-05-14 16:18:55 +0900

    가계부(실행)

--- java_yme/src/accountbook/AccountBookTest.java
+++ java_yme/src/accountbook/AccountBookTest.java
@@ -5,17 +5,50 @@ import java.util.Scanner;
 public class AccountBookTest {
 
 	public static void main(String[] args) {
-		/* 가계부 프로그램 구현 하려고 한다.
-		 * 이때 필요한 클래스를 만들어 보세요.
-			종류 : 수입 / 지출
-			날짜 : (1~30)
-			자산 : 현금/은행/카드
-			분류 : 교통비 / 식비 등
-			금액 : xx원
-			내역 : 교통비 (버스비)
-		 * */
-	}
-
-
-	
+		AccountBookProgram abp = new AccountBookProgram();
+		Scanner scan = new Scanner(System.in);
+		int menu=0;
+		int subMenu=0;
+		do {
+			abp.printMenu();
+			menu= scan.nextInt();
+			switch(menu) {
+				case 1 : 
+					abp.insertAcBook();
+					break;
+				case 2 :
+					abp.updateAcBook(null);
+					break;
+				case 3 :
+					abp.deleteAcBook();
+					break;
+				case 4 :
+					abp.printSynMenu();
+					subMenu= scan.nextInt();
+					switch(subMenu) {
+						case 1:
+							abp.detailList();
+							break;
+						case 2:
+							abp.simpleList();
+							break;
+						case 3:
+							abp.printItem();
+							break;
+						case 4:
+							abp.printTotal();
+							break;
+						default:
+							System.out.println("잘못된 메뉴");
+					}
+					break;
+				case 5 :
+					System.out.println("프로그램 종료"); 
+					break;
+				default: System.out.println("잘못된 메뉴입니다.");	
+			}
+		}while(menu!=5);
+		scan.close();
+	} 
 }
+
