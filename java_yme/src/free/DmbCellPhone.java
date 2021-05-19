fda2a668b1fddf75bbcb51824217c3b311687e07
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-05-20 00:10:34 +0900

    상속 복습

---
+++ java_yme/src/free/DmbCellPhone.java
@@ -0,0 +1,25 @@
+package free;
+
+public class DmbCellPhone extends CellPhone{
+	//필드
+	int channel;
+	
+	//생성자
+	DmbCellPhone(String model, String color, int channel){
+		//super();
+		this.model = model;
+		this.color = color;
+		this.channel = channel;
+	}
+	
+	//메소드
+	void turnOnDmb() {
+		System.out.println("체널 " + channel + "번 DMB 방송 수신을 시작합니다.");
+	}
+	void changeChannelDmb(int channel) {
+		System.out.println("체널 " + channel + "번으로 바꿉니다.");
+	}
+	void turnOffDmb() {
+		System.out.println("DMB 방송 수신을 멈춥니다.");
+	}
+}
--- java_yme/src/free/Student.java
+++ java_yme/src/free/Student.java
@@ -1,4 +1,10 @@
 package free;
 
-public class Student {
+public class Student extends People{
+	public int studentNo;
+	
+	public Student(String name,String ssn, int studentNo) {
+		super(name,ssn);
+		this.studentNo = studentNo;
+	}
 } 
