3bb3729a0c5a5631bbe79dca62594c3ecb2c5d1e
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-05-25 23:42:12 +0900

    복습

---
+++ java_yme/src/free/RemoteControl.java
@@ -0,0 +1,11 @@
+package free;
+
+public interface RemoteControl {
+	
+	public int MAX_VOLUME = 10;
+	public int MIN_VOLUME = 1;
+	
+	public void turnOn();
+	public void turnOff();
+	public void setVolume(int volume);
+}
