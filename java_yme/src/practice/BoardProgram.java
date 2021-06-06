6e4cb596f0650f239dcd32c676dfa9811679c090
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-06-06 11:25:01 +0900

    복습

---
+++ java_yme/src/practice/BoardProgram.java
@@ -0,0 +1,40 @@
+package practice;
+
+public class BoardProgram implements BoardService{
+
+	@Override
+	public void insert(Board board) {
+		
+	}
+
+	@Override
+	public boolean delete(int num) {
+
+		return false;
+	}
+
+	@Override
+	public void update(Board board) {
+		
+		
+	}
+
+	@Override
+	public Board getBorard(int num) {
+		
+		return null;
+	}
+
+	@Override
+	public Board[] getBoardList() {
+		
+		return null;
+	}
+
+	@Override
+	public Board[] getBoardList(String search) {
+		
+		return null;
+	}
+
+}
