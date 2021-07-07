0a2eef18b5293672e9cf2b696ce72e6aed9a2b0f
yme1313 <83192170+yme1313@users.noreply.github.com>
2021-07-07 22:26:32 +0900

    수정

--- java_yme/쿼리문/spring_yme쿼리문/spring_yme_board.sql
+++ java_yme/쿼리문/spring_yme쿼리문/spring_yme_board.sql
@@ -33,7 +33,7 @@ CREATE TABLE `board` (
   PRIMARY KEY (`num`),
   KEY `writer_idx` (`writer`),
   CONSTRAINT `writer` FOREIGN KEY (`writer`) REFERENCES `member` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,6 +42,7 @@ CREATE TABLE `board` (
 
 LOCK TABLES `board` WRITE;
 /*!40000 ALTER TABLE `board` DISABLE KEYS */;
+INSERT INTO `board` VALUES (1,'제목','내용','abc123','2021-07-07 13:49:42',5,'I'),(2,'제목2','내용2','abc123','2021-07-07 13:49:42',5,'D'),(40,'7월 7일','게시글 등록 test3','abc123','2021-07-07 14:09:55',2,'I'),(41,'게시글 수정','수정 test','abc123','2021-07-07 14:49:22',3,'D'),(42,'네이버페이 포인트 적립 문의입니다.','mll','abc123','2021-07-07 20:31:22',1,'I');
 /*!40000 ALTER TABLE `board` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-05 15:18:05
+-- Dump completed on 2021-07-07 22:25:37
